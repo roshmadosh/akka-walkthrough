e0bd9225b4851dd1ad4f492a55613228bfe88c7e
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-04-14 15:17:50 -0400

    recordTemperature command for Device actor with test

--- src/main/java/com/example/iot/Device.java
+++ src/main/java/com/example/iot/Device.java
@@ -2,6 +2,8 @@ package com.example.iot;
 
 import java.util.Optional;
 
+import com.example.iot.Device.RespondTemperature.TemperatureRecorded;
+
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.PostStop;
@@ -9,6 +11,7 @@ import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
+import scala.collection.immutable.ArraySeq.ofInt;
 
 public class Device extends AbstractBehavior<Device.Command> {
 	private final String groupId;
@@ -18,7 +21,7 @@ public class Device extends AbstractBehavior<Device.Command> {
 	
 	public interface Command {}
 
-	// read message
+	// readTemp request
 	public static final class ReadTemperature implements Command {
 		final long requestId;
 		final ActorRef<RespondTemperature> replyTo;
@@ -30,7 +33,7 @@ public class Device extends AbstractBehavior<Device.Command> {
 
 	}
 
-	// response message
+	// readTemp response
 	public static final class RespondTemperature {
 		final long requestId;
 		final Optional<Double> value;
@@ -41,6 +44,29 @@ public class Device extends AbstractBehavior<Device.Command> {
 
 		}	
 
+	}
+
+	// writeTemp request
+	public static final class RecordTemperature implements Command {
+		final long requestId;
+		final double value;
+		final ActorRef<TemperatureRecorded> replyTo;
+		
+		public RecordTemperature(long requestId, double value, ActorRef<TemperatureRecorded> replyTo) {
+			this.requestId = requestId;
+			this.value = value;
+			this.replyTo = replyTo;
+		}
+	}
+
+
+	// writeTemp response
+	public static final class TemperatureRecorded {
+		final long requestId;
+
+		public TemperatureRecorded(long requestId) {
+			this.requestId = requestId;
+		}
 	}
 
 	// factory method
@@ -63,8 +89,16 @@ public class Device extends AbstractBehavior<Device.Command> {
 	public Receive<Command> createReceive() {
 		return newReceiveBuilder()
 			.onMessage(ReadTemperature.class, this::onReadTemperature)
+			.onMessage(RecordTemperature.class, this::onRecordTemperature)
 			.onSignal(PostStop.class, signal -> onPostStop())
 			.build();
+	}
+
+	private Behavior<Command> onRecordTemperature(RecordTemperature  r) {
+		getContext().getLog().info("Updating temperature reading to {} with {}", r.value, r.requestId);
+		lastTemperatureReading = Optional.of(r.value);
+		r.replyTo.tell(new TemperatureRecorded(r.requestId));
+		return this;
 	}
 
 	private Behavior<Command> onReadTemperature(ReadTemperature r) {
--- src/test/java/com/example/iot/DeviceTest.java
+++ src/test/java/com/example/iot/DeviceTest.java
@@ -25,7 +25,29 @@ public class DeviceTest {
 
 		assertEquals(40L, response.requestId);
 		assertEquals(Optional.empty(),response.value);
+	}
 
 
+	@Test
+	public void testReplyWithLatestTemperatureReading() {
+		TestProbe<Device.TemperatureRecorded> recordProbe = testKit.createTestProbe(Device.TemperatureRecorded.class);
+		TestProbe<Device.RespondTemperature> readProbe = testKit.createTestProbe(Device.RespondTemperature.class);
+		ActorRef<Device.Command> deviceActor = testKit.spawn(Device.create("group", "device"));
+
+		deviceActor.tell(new Device.RecordTemperature(1L, 24.0, recordProbe.getRef()));
+		assertEquals(1L, recordProbe.receiveMessage().requestId);
+
+		deviceActor.tell(new Device.ReadTemperature(2L, readProbe.getRef()));
+		Device.RespondTemperature response1 = readProbe.receiveMessage();
+		assertEquals(2L, response1.requestId);
+		assertEquals(Optional.of(24.0), response1.value);
+
+		deviceActor.tell(new Device.RecordTemperature(3L, 55.0, recordProbe.getRef()));
+		assertEquals(3L, recordProbe.receiveMessage().requestId);
+
+		deviceActor.tell(new Device.ReadTemperature(4L, readProbe.getRef()));
+		Device.RespondTemperature response2 = readProbe.receiveMessage();
+		assertEquals(4L, response2.requestId);
+		assertEquals(Optional.of(55.0), response2.value);
 	}
 }
